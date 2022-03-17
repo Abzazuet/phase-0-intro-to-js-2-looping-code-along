de13828ce788242adcab7ba5d094b219aaced280
Abel Zazueta <abel.zazueta@gmail.com>
2022-03-17 14:05:50 -0700

    Tests passed

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(names, eventName){
+    let message = '';
+    const arr = [];
+    for (let i in names){
+        message = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+        arr.push(message);
+    }
+    return arr;
+}
+function countDown(number){
+    while (number>=0){
+        console.log(number);
+        number--;
+    }
+}
